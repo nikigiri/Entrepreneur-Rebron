762d8d1f1579c477a4534db55ef2b1f640a1d248
davepaningo <davepaningo2@gmail.com>
2025-12-18 15:31:52 +0700

    user

--- User.java
+++ User.java
@@ -1,3 +1,25 @@
-public class User {
-    
+public abstract class User {
+    protected String id;
+    protected String nama;
+    protected String email;
+    protected String password;
+
+    public User(String id, String nama, String email, String password) {
+        this.id = id;
+        this.nama = nama;
+        this.email = email;
+        this.password = password;
+    }
+
+    public boolean login(String email, String password) {
+        return this.email.equals(email) && this.password.equals(password);
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public abstract void menu();
 }
+
+
