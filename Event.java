ce9bf0de4706289214b661cfbe0ff3ccfd1f52c0
muthialuthfinurwindi <muthialuthfi.@icloude.com>
2025-12-18 16:19:57 +0700

    tambah event dan panitia

--- Event.java
+++ Event.java
@@ -1,3 +1,36 @@
+import java.time.LocalDate;
+
 public class Event {
-    
+    private String idEvent;
+    private String namaEvent;
+    private LocalDate tanggalEvent;
+    private String lokasi;
+    private String deskripsi;
+    private int kuota;
+
+    public Event(String idEvent, String namaEvent, LocalDate tanggalevent, String lokasi, String deskripsi, int kuoat) {
+        this.idEvent = idEvent;
+        this.namaEvent = namaEvent;
+        this.tanggalEvent = tanggalEvent;
+        this.lokasi = lokasi;
+        this.deskripsi = deskripsi;
+        this.kuota = kuota;
+
+    }
+
+    public boolean cekKuota() {
+        return kuota > 0;
+    }
+
+    public void kurangiKuota() {
+        kuota--;
+    }
+
+    public String getNamaEvent() {
+        return namaEvent;
+    }
+
+    public int getKuota() {
+        return kuota;
+    }
 }
--- Panitia.java
+++ Panitia.java
@@ -1,3 +1,25 @@
-public class Panitia {
-    
+public class Panitia extends User {
+    private String jabatan;
+
+    public Panitia(String id, String nama, String email, String password, String jabatan) {
+        super(id, nama, email, password);
+        this.jabatan = jabatan;
+    }
+
+    public void tambahEvent(Event e) {
+        System.out.println("Event " + e.getNamaEvent() + " ditambahkan");
+    }
+
+    public void ubahEvent(Event e) {
+        System.out.println("Event " + e.getNamaEvent() + " diubah");
+    }
+
+    public void hapusEvent(String idEvent) {
+        System.out.println("Event dengan ID " + idEvent + " dihapus");
+    }
+
+    public boolean verifikasiPembayaran(Pembayaran p) {
+        p.setStatusPembayaran("Terverifikasi");
+        return true;
+    }
 }
