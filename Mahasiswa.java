49f7d6e898ebb59321893674c9ae91397b579f27
nikigiri <giriniki54@gmail.com>
2025-12-18 15:42:58 +0700

    Tambah Mahasiswa

---
+++ Mahasiswa.java
@@ -0,0 +1,18 @@
+public class Mahasiswa extends User {
+    private String Fakultas;
+    private String programStudi;
+
+    public Mahasiswa(String id, String nama, String email, String password, String Fakultas, String programStudi) {
+        super(id, nama, email, password);
+        this.Fakultas = Fakultas;
+        this.programStudi = programStudi;
+    }   
+    public void daftarEvent(Event e) {
+        if (e.cekKuota()) {
+            System.out.println("berhasil mendaftar Event: " + e.getNamaEvent()  );
+            e.kurangiKuota();   
+        } else {
+            System.out.println("Gagal mendaftar, kuota Event penuh");
+        }
+    }
+}
